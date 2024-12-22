6b51a70fccf3f99e3e96c38ac5a042b4fcabc517
saimanish0 <psaimanish23@outlook.com>
2024-12-22 20:27:23 +0530

    Small Change

--- App.tsx
+++ App.tsx
@@ -1,5 +1,11 @@
+import * as Yup from 'yup';
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+
+const PasswordScheme = Yup.object().shape({
+  passwordLength: Yup.number().min(4).max(10).required(),
+  confirmPassword: Yup.number().min(4).max(10).required(),
+});
 
 export default function App() {
   return (
